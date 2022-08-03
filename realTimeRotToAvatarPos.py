99685d1467defc60c6824ed1fd3fd0bf1f7acbe3
LouisLiang <a8945267@gmail.com>
2022-08-03 12:37:39 +0800

    完成kinematic chain的visualize
    TODO: 完成FK function的實作

--- realTimeRotToAvatarPos.py
+++ realTimeRotToAvatarPos.py
@@ -8,6 +8,9 @@ import json
 import pickle
 from positionAnalysis import jointsNames
 from realTimeHandRotationCompute import jointsNames as handJointNames
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
 
 usedLowerBodyJoints = [
     jointsNames.LeftUpperLeg, jointsNames.LeftLowerLeg, jointsNames.LeftFoot, 
@@ -26,6 +29,43 @@ def loadTPosePosAndVecs(saveDirPath):
         TPoseVectors = pickle.load(inPickle)
     return TPosePositions, TPoseVectors
 
+def visualize3DVecs(startPts, vectors):
+    '''
+    (original version is in realTimeHandRotationCompute)
+    Goal: 顯示3D空間向量, 方便debug, 顯示部分joints以及vertex即可
+    Input: 
+    :*args: 多個3d position array組成的list代表向量的起點 + 多個3d position array組成的list代表多個向量
+            以上兩者長度要相同
+    '''
+    # origin = [0, 0, 0]
+    # x = [1, 2, 3]
+    # y = [4, 5, 6]
+    # xCy = np.cross(x, y)
+    xyzZip = list(zip(*[startPts[i]+vectors[i] for i in range(len(vectors))]))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    colormap = matplotlib.cm.inferno
+    colors = list(range(len(vectors)))
+    norm = matplotlib.colors.Normalize()
+    norm.autoscale(colors)
+    for i in range(len(vectors)):
+        ax.quiver(xyzZip[0][i], xyzZip[1][i], xyzZip[2][i], xyzZip[3][i], xyzZip[4][i], xyzZip[5][i], arrow_length_ratio =0.2, color=colormap(norm(colors[i])), label=i)
+    ax.set_xlim([0, 2])
+    ax.set_ylim([0, 2])
+    ax.set_zlim([0, 2])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    plt.legend()
+
+def forwardKinematic():
+    '''
+    Goal: 給定一個kinematic chain(1 position, 2 vectors), 
+        給予第一個joint的X, Z旋轉, 以及第二個joint的X旋轉, 
+        求第2及第3個點的旋轉後position
+    '''
+    pass
+
 if __name__=='__main__':
     # 1. 讀取預存好的T pose position以及vectors
     # 2. 讀取mapped hand rotations
@@ -42,11 +82,37 @@ if __name__=='__main__':
     mappedHandRotJson = None
     with open(mappedHandRotSaveDirPath+'leftFrontKick(True, False, False, False, True, True).json', 'r') as fileIn:
         mappedHandRotJson = json.load(fileIn)
-    print(mappedHandRotJson)
+    # print(mappedHandRotJson)
     # 3. 
-    # TODO: visualize 三個joints的heirarchy結構
+    # TODO: visualize 三個joints的heirarchy結構(origin position, two vectors)
     # TODO: 先嘗試寫寫看三個joints的heirarchy結構旋轉後會發生甚麼事
+    # TODO: 原始的XYZ軸與現在的相同嗎? 要如何校正?
     # 這邊只會有兩個獨立的heirarchy結構: 左腿, 右腿
+    ## 3.1 切成兩個kinematic chain(left and right), 並且接下來的處理都是以chain為單位
+    leftKinematic = [
+        TPosePositions[jointsNames.LeftUpperLeg], 
+        TPoseVectors[0], 
+        TPoseVectors[1]
+    ]  # upper leg position, upper leg vector, lower leg vector
+    rightKinematic = [
+        TPosePositions[jointsNames.RightUpperLeg], 
+        TPoseVectors[2], 
+        TPoseVectors[3]
+    ]
+    visualize3DVecs(
+        [leftKinematic[0].tolist(), (leftKinematic[0]+leftKinematic[1]).tolist()], 
+        [leftKinematic[1].tolist(), leftKinematic[2].tolist()]
+    )
+    visualize3DVecs(
+        [leftKinematic[0].tolist(), (leftKinematic[0]+leftKinematic[1]).tolist()], 
+        [leftKinematic[1].tolist(), leftKinematic[2].tolist()]
+    )
+    # visualize3DVecs(
+    #     [[0,0,0], [0,0,1]], 
+    #     [[0,1,0], [0,1,1]]
+    # )
+    plt.show()
+    ## 3.2 
 
 if __name__=='__main01__':
     # 1. 讀取檔案, 得到TPose狀態下的position資訊
