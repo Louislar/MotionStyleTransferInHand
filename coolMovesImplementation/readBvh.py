f31eba38ca11c3befdd431021f6d407e73345a4a
louisLiang <a8945267@gmail.com>
2022-09-23 16:52:37 +0800

    完成處理bvh轉3dPos程式
    目前還未將一次處裡多個檔案的程式撰寫完成
    目前只有處理好125_01, 125_02的bvh檔案

--- coolMovesImplementation/readBvh.py
+++ coolMovesImplementation/readBvh.py
@@ -1,35 +1,75 @@
+import os
 import matplotlib.pyplot as plt 
 from pymo.parsers import BVHParser
 from pymo.viz_tools import *
 from pymo.preprocessing import *
 
-def extractDataFromMocapData():
+def extractDataFromMocapData(mocapDataDf, targetJoints):
     '''
     TODO: 
     MocapData is a data structure defined in the pymo library. 
     I only want to extract the XYZ positions from some joints in the MocapData. 
-    Easy access points is the MocapData.values is a pd.DataFrame. 
+    Easy access points is that the MocapData.values is a pd.DataFrame. 
 
     The desire joints: 
     1. left/right feet
     '''
-    pass
+    jointsDataDict = {_nm: None for _nm in targetJoints}
+    for _nm in targetJoints:
+        xPosColNM = _nm + '_Xposition'
+        yPosColNM = _nm + '_Yposition'
+        zPosColNM = _nm + '_Zposition'
+        jointsDataDict[_nm] = mocapDataDf[[xPosColNM, yPosColNM, zPosColNM]].rename(columns={
+            xPosColNM: 'x', yPosColNM: 'y', zPosColNM: 'z'
+        })
+    return jointsDataDict
 
-parser = BVHParser()
 
-parsed_data = parser.parse('data/swimming/125/125_01.bvh')
 
-print_skel(parsed_data)
+bvhFilePath = 'data/swimming/125/125_02.bvh'
+# TODO: 一次處理多個bvh檔案的轉檔
+bvhFiles = []
 
-mp = MocapParameterizer('position')
+if __name__=='__main__':
 
-positions = mp.fit_transform([parsed_data])
+    # Parse file path
+    bvhFileNM = os.path.basename(bvhFilePath)
+    bvhFileNM_withoutEXT = os.path.splitext(bvhFileNM)[0]
+    bvhDirPath = os.path.dirname(bvhFilePath)
+    bvhDirParentPath = os.path.dirname(bvhDirPath)
+    bvhDirNM = os.path.basename(bvhDirPath)
+    print('Read bvh directory path: ', bvhDirPath)
 
-print(type(positions))
-print(type(positions[0].values))
-print(positions[0].values.shape)
-print(positions[0].values.columns)
+    # Parse bvh
+    parser = BVHParser()
+    parsed_data = parser.parse(bvhFilePath)
+    print_skel(parsed_data)
+    mp = MocapParameterizer('position')
 
-draw_stickfigure(positions[0], frame=10)
+    positions = mp.fit_transform([parsed_data])
 
-# plt.show()
+    print(type(positions))
+    print(len(positions))
+    print(type(positions[0].values))
+    print(positions[0].values.shape)
+    print(positions[0].values.columns)
+    print(positions[0].skeleton.keys())
+
+    # Parse data into 3d positions time series
+    jointsPosData = extractDataFromMocapData(positions[0].values, list(positions[0].skeleton.keys()))
+    print(jointsPosData['Hips'].head(10))
+
+    # Save data in files
+    saveDirPath = 'data/swimming/125_parsed/'
+    saveDirPath = os.path.join(bvhDirParentPath, bvhDirNM+'_parsed')
+    saveDirPath = os.path.join(saveDirPath, bvhFileNM_withoutEXT)
+    print('Save directory path: ', saveDirPath)
+    for _jointNM in jointsPosData.keys():
+        print('saved: ', os.path.join(saveDirPath, _jointNM+'.csv'))
+        jointsPosData[_jointNM].to_csv(os.path.join(saveDirPath, _jointNM+'.csv'), index=False)
+        pass
+
+
+    # draw_stickfigure(positions[0], frame=10)
+
+    # plt.show()
